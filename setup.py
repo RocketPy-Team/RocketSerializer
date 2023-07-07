710be1d59f9c86d728c3fc4d82d3d2f708b6ddfd
Guilherme <guilherme_fernandes@usp.br>
2023-07-07 17:36:05 +0200

    MAINT: update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+__author__ = "RocketPy Team"
+__credits__ = [
+    "Patrick Sampaio Brandão",
+    "Franz Masatoshi Yuri",
+    "Guilherme Fernandes Alves",
+]
+__license__ = "MIT"
+__author_email__ = "rocketpyteam@gmail.com"
+__copyright__ = "RocketPy Team, 20XX, all rights reserved."
+
 from setuptools import setup
 
 setup(
@@ -8,6 +18,15 @@ setup(
     install_requires=["bs4", "click", "lxml", "numpy", "orhelper", "pyyaml"],
     entry_points="""
         [console_scripts]
-        serializer=open_rocket_serializer.cli:cli
+        rocket-serializer=open_rocket_serializer.cli:cli
+        ork2json=open_rocket_serializer.cli:ork2json
+        ork2py=open_rocket_serializer.cli:ork2py
+        ork2ipynb=open_rocket_serializer.cli:ork2ipynb
     """,
+    author=__author__,
+    author_email=__author_email__,
+    credits=__credits__,
+    license=__license__,
+    description="A serializer for OpenRocket files",
+    homepage="https://github.com/RocketPy-Team/OpenRocketSerializer",
 )
