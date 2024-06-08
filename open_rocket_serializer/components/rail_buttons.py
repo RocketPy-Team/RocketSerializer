e03278cfce3ac1dedc32b5e39d31911e0ac4c32d
Gui-FernandesBR <guilherme_fernandes@usp.br>
2024-06-08 14:13:27 -0300

    ENH: Implement the `search_rail_buttons` function

--- open_rocket_serializer/components/rail_buttons.py
+++ open_rocket_serializer/components/rail_buttons.py
@@ -1,2 +1,38 @@
-# launchlug
-# radialdirection
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def search_rail_buttons(bs, elements: dict) -> dict:
+
+    lugs_elements = []
+    for key, value in elements.items():
+        if value["type"] == "LaunchLug":
+            lugs_elements.append(value)
+
+    if len(lugs_elements) < 2 or not lugs_elements:
+        logger.info("Could not fetch a pair of rail buttons")
+        return {}
+
+    lugs_elements.sort(key=lambda x: x["position"])
+    # We only need the 2 buttons closest to the nozzle.
+    lugs_elements = lugs_elements[-2:]
+
+    upper_position = lugs_elements[0]["position"]
+    lower_position = lugs_elements[1]["position"]
+    name = str(lugs_elements[0]["name"])
+
+    angular_position = 0.0
+    lugs = bs.findAll("launchlug")
+    for lug in lugs:
+        if lug.find("name").text == name:
+            angular_position = float(lug.find("radialdirection").text)
+            break
+
+    return {
+        "name": str(name),
+        "upper_position": upper_position,
+        "lower_position": lower_position,
+        "distance": lower_position - upper_position,
+        "angular_position": angular_position,
+    }
