d85b69fb981540c3611f7dbfdd8b8139014878a9
Gui-FernandesBR <guilherme_fernandes@usp.br>
2024-06-11 22:35:21 -0300

    TST: Add file for debugging purposes

---
+++ test_ork2json-debugger.py
@@ -0,0 +1,9 @@
+from open_rocket_serializer.cli import ork2json
+
+# NOTE: Only works if you comment the decorators in the ork2json function.
+# NOTE: use this to run the python debugger
+# NOTE: restart the jupyter kernel if needed, so JVEM can restart
+
+ork2json(
+    "examples/databank/Team24/rocket.ork",
+)
