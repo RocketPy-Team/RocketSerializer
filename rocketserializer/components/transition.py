eb02fafb4b785a0e224bd80cab37f6a383747f4d
Gui-FernandesBR <guilherme_fernandes@usp.br>
2026-07-04 20:20:04 -0300

    Fix: Ensure bottom_radius is float in transition component

--- rocketserializer/components/transition.py
+++ rocketserializer/components/transition.py
@@ -68,20 +68,20 @@ def search_transitions(bs, elements, ork):
 
         if transition_ork is not None:
             top_radius = float(transition_ork.getForeRadius())
+            bottom_radius = float(transition_ork.getAftRadius())
         else:
             logger.warning(
-                "Could not find Java transition for '%s', using foreradius from XML.",
+                "Could not find Java transition for '%s', using radii from XML.",
                 label,
             )
             fore_tag = transition.find("foreradius")
             fore_text = fore_tag.text if fore_tag else "0"
             top_radius = 0.0 if "auto" in fore_text else float(fore_text)
 
-        bottom_radius = (
-            transition.find("aftradius").text
-            if "auto" in transition.find("aftradius").text
-            else float(transition.find("aftradius").text)
-        )
+            aft_tag = transition.find("aftradius")
+            aft_text = aft_tag.text if aft_tag else "0"
+            bottom_radius = 0.0 if "auto" in aft_text else float(aft_text)
+
         length = float(transition.find("length").text)
         logger.info("Collected the dimensions of the transition number %d", idx)
 
